4ca5abc8b1b16d88e20caf3a93da5c6d8133d1f0
asaezp24 <asaezp24@fpsanturtzilh.eus>
2026-02-18 12:43:48 +0100

    NOSE

--- ERRONKA2/Bezeroa/ChatForm/obj/Debug/net8.0-windows/ChatForm.AssemblyInfo.cs
+++ ERRONKA2/Bezeroa/ChatForm/obj/Debug/net8.0-windows/ChatForm.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ChatForm")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c29fbb56d9dd25e4e3a0d7f6cc8c37a98d407af5")]
 [assembly: System.Reflection.AssemblyProductAttribute("ChatForm")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ChatForm")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
